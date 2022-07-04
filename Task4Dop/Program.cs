10bc65b61e15951dd1cd1f8fcff6f83c785e4b0a
Tatiana <ttt-ttt@bk.ru>
2022-07-04 04:30:27 +0300

    Task4Dop

---
+++ Task4Dop/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 4. Дан массив длиной 10 элементов. Заполнить его последовательно числами от 1 до 10.
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] collection)
+{
+    int length = 10;
+    int index = 0;
+    int num = 1;
+    while (index < length)
+    {
+        collection[index] = num;
+        index++;
+        num++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = 10;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(col[position] + " ");
+        position++;
+    }
+    Console.WriteLine(" ");
+}
+
+
