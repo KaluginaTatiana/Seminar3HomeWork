b9a07c221ce9a2c5b9e1942ee91d98906a4f2c49
Tatiana <ttt-ttt@bk.ru>
2022-07-06 02:06:24 +0300

    the third grade of a number from 1 to N

---
+++ Task23/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Введите целое положительное число, не равное нулю.");
+double n = Convert.ToDouble(Console.ReadLine());
+if ( n < 1 || n % 1 != 0 )
+Console.WriteLine("Ошибка. Введите целое положительное число, не равное нулю.");
+else 
+SqrNumbers(n);
+
+void SqrNumbers(double number)
+{
+    double sqr = 1;
+    while (sqr <= number)
+    {
+        Console.Write(Math.Pow(sqr, 3) + " ");
+        sqr++;
+    }
+    Console.WriteLine(" ");
+}
