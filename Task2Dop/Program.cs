89e455334057066a62197bd1edb0793ed06b6242
Tatiana <ttt-ttt@bk.ru>
2022-07-06 19:06:54 +0300

    upgrade

--- Task2Dop/Program.cs
+++ Task2Dop/Program.cs
@@ -1,9 +1,13 @@
 ﻿// Задача 2. Дано трёхзначное число N. Определить кратна ли трём сумма всех его цифр.
 Console.WriteLine("Введите трехзначное число.");
 int N = Convert.ToInt32(Console.ReadLine());
-int sum = N % 10 + (N / 10 % 10) + N / 100;
-if (sum % 3 == 0)
-Console.WriteLine("Сумма цифр числа " + N + " кратна 3.");
+if (N > 999 | N < 100)
+    Console.WriteLine("Ошибка. Число должно быть трехзначным");
 else
-Console.WriteLine("Сумма цифр числа " + N + " не кратна 3.");
-
+{
+    int sum = N % 10 + (N / 10 % 10) + N / 100;
+    if (sum % 3 == 0)
+        Console.WriteLine("Сумма цифр числа " + N + " кратна 3.");
+    else
+        Console.WriteLine("Сумма цифр числа " + N + " не кратна 3.");
+}
